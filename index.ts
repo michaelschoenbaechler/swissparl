4f1304f92d3cc84a185252ae4701ea89b77620e4
Michael Schönbächler <m.schoenbaehler@icloud.com>
2022-09-03 17:04:41 +0200

    add eslint

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-const world = 'world';
+const world = 'world'
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
+export function hello (who: string = world): string {
+  return `Hello ${who}! `
 }
