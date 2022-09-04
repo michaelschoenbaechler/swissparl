ebd931ac0634e374f688b57ae6cdd2660158f9d3
Michael Schönbächler <m.schoenbaehler@icloud.com>
2022-09-04 16:26:09 +0200

    configure prettier and eslint

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,38 @@
-import { OData, PlainODataMultiResponse, ODataQueryParam } from "@odata/client";
-import { Collection, Person, SwissParlEntity } from "./models";
+import { OData, ODataFilter, PlainODataMultiResponse } from "@odata/client";
+import { Collection, Party, SwissParlEntity } from "./models";
 
 const serviceUrl = "https://ws.parlament.ch/odata.svc/$metadata";
 const client = OData.New({
   metadataUri: serviceUrl,
 });
 
-export function createFilterParam<T extends SwissParlEntity>(
-  entity: T
-): ODataQueryParam {
-  const filter = client.newFilter();
-  for (const key in entity) {
-    const value = entity[key] as string;
-    filter.property(key).eq(value);
-  }
-  return client.newParam().filter(filter);
+interface RequestOptions<T extends SwissParlEntity> {
+  filter?: T;
+  expand?: string;
 }
 
-export async function getEntities<T>(
+function createFilter<T>(filterProperties: T): ODataFilter {
+  const filter = client.newFilter();
+  for (const key in filterProperties) {
+    const value = filterProperties[key] as string;
+    filter.property(key).eq(value);
+  }
+  return filter;
+}
+
+export async function queryCollection<T extends SwissParlEntity>(
   collection: Collection,
-  params: ODataQueryParam
+  options: RequestOptions<T>
 ): Promise<T[]> {
+  const params = client.newParam();
+  if (options.filter !== undefined) {
+    params.filter(createFilter<T>(options.filter));
+  }
+
+  if (options.expand !== undefined) {
+    params.expand(options.expand);
+  }
+
   const entities: PlainODataMultiResponse = await client.newRequest<T>({
     collection,
     params,
@@ -30,10 +42,10 @@ export async function getEntities<T>(
 }
 
 const runner = async (): Promise<void> => {
-  const person = await getEntities<Person>(
-    Collection.Person,
-    createFilterParam<Person>({ Language: "DE", NumberOfChildren: 0 })
-  );
+  const person = await queryCollection<Party>(Collection.Party, {
+    filter: { Language: "DE" },
+    expand: "MembersParty",
+  });
   console.log(person);
 };
 
--- src/models.ts
+++ src/models.ts
@@ -1,5 +1,7 @@
 export enum Collection {
   Person = "Person",
+  Party = "Party",
+  MemberParty = "MemberParty",
 }
 
 export interface SwissParlEntity {
@@ -9,4 +11,14 @@ export interface SwissParlEntity {
 export interface Person extends SwissParlEntity {
   Language?: string;
   NumberOfChildren?: number;
+}
+
+export interface Party extends SwissParlEntity {
+  Language?: string;
+  PartyAbbreviation?: string;
+}
+
+export interface MemberParty extends SwissParlEntity {
+  Language?: string;
+  PartyAbbreviation?: string;
 }
