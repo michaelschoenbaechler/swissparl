43b42a2c5f33e525326fe0c19fad500b0833834f
Michael Schönbächler <m.schoenbaehler@icloud.com>
2022-10-08 00:12:01 +0200

    clean up

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { OData, ODataFilter, PlainODataMultiResponse } from "@odata/client";
+import { Collection, SwissParlEntity } from "./models";
+
+const serviceUrl = "https://ws.parlament.ch/odata.svc/$metadata";
+const client = OData.New({
+  metadataUri: serviceUrl,
+});
+
+interface RequestOptions<T extends SwissParlEntity> {
+  filter?: T;
+  expand?: string;
+}
+
+function createFilter<T>(filterProperties: T): ODataFilter {
+  const filter = client.newFilter();
+  for (const key in filterProperties) {
+    const value = filterProperties[key] as string;
+    filter.property(key).eq(value);
+  }
+  return filter;
+}
+
+export async function queryCollection<T extends SwissParlEntity>(
+  collection: Collection,
+  options: RequestOptions<T>
+): Promise<T[]> {
+  const params = client.newParam();
+  if (options.filter !== undefined) {
+    params.filter(createFilter<T>(options.filter));
+  }
+
+  if (options.expand !== undefined) {
+    params.expand(options.expand);
+  }
+
+  const entities: PlainODataMultiResponse = await client.newRequest<T>({
+    collection,
+    params,
+  });
+
+  return entities.d != null ? entities.d?.results : [];
+}
